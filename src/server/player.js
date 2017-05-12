314bf518b8ca30a1deaa3b173eeddd648000bc5d
Zack Mattor <zmattor@gmail.com>
2017-05-11 23:45:29 -0400

    rename player

---
+++ src/server/player.js
@@ -0,0 +1,46 @@
+const EventEmitter = require('events');
+
+class Player extends EventEmitter {
+  constructor(con) {
+    super();
+
+    console.log('New Player Instance');
+
+    this.con = con;
+    this.key_state = null;
+
+    con.on('close', this._shutdown_instance.bind(this))
+    con.on('bind_attempt', this._bind_attempt.bind(this))
+    con.on('key_state', this._new_key_state.bind(this))
+  }
+
+  bind_status(bind_successful) {
+    console.log(`Did we bind? - ${bind_successful}`);
+    this.con.send('bind_status', {was_successful: bind_successful});
+  }
+
+  get_key_state(key_name) {
+    if(!this.key_state) return false;
+
+    return this.key_state[key_name];
+  }
+
+  // Private
+
+  _new_key_state(key_state) {
+    console.log("Recieved key state from controller!");
+    console.log(key_state);
+    this.key_state = key_state;
+  }
+
+  _bind_attempt(data) {
+    this.emit('bind_attempt', {token: data.token, instance: this});
+  }
+
+
+  _shutdown_instance() {
+    this.emit('close', this.con.id);
+  }
+}
+
+module.exports = Player;
